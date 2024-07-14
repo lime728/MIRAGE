40a1fe78258035fc51128ee144b3d8e7381ce4e0
lime728 <141900390+lime728@users.noreply.github.com>
2024-07-14 15:49:42 +0800

    Update config.py

--- config.py
+++ config.py
@@ -12,24 +12,18 @@ ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
 class Config:
     # Manual Configs
-    ScriptNumList = [0, 1, 2, 3, 4, 5, 6, 7]    # Chosen Script Numbers to run (From 0 to 7)
-    ScriptNum = 0                               # Chosen Script Number (From 0 to 7)
+    ScriptNumList = [0, 1, 2, 3, 4, 5, 6, 7]    # Chosen Script Numbers to run (From 0 to 7)  -- LOWER PRIORITY!
+    ScriptNum = 0                               # Chosen Script Number (From 0 to 7 and None) -- HIGHER PRIORITY!
     MaxTurnNum = 5                              # Runnable Turn Number
     MaxRetries = 20                             # Max Retry in requests
     MaxBaseScriptSummaryToken = 1500            # Max token length of base script summary
-    DEBUG = False                               # debug mode
-    Console = True                              # console mode
-    # Model = 'gpt-3.5-turbo-0125'                # Api-based model
-    Model = 'gpt-4-1106-preview'                # Api-based model
-    # Model = 'gpt-4-turbo'                       # Api-based model
-    # Model = 'gpt-4o'                            # Api-based model
+    DEBUG = False                               # debug mode (Output all logs)
+    Console = True                              # console mode (Only output conversation contents)
+    Model = 'gpt-3.5-turbo-0125'                # Api-based model
     force_summary = True                        # Use force summary or not
     # Eval Configs
-    # Eval_Model = 'gpt-3.5-turbo-0125'           # Eval Api-based model
-    Eval_Model = 'gpt-4-1106-preview'           # Eval Api-based model
-    # Eval_Model = 'gpt-4-turbo'                  # Eval Api-based model
-    # Eval_Model = 'gpt-4o'                       # Eval Api-based model
-    Only_Eval = True                            # Eval with exist history script
+    Eval_Model = 'gpt-4-turbo'                  # Eval Api-based model
+    Only_Eval = False                           # Eval with exist history script
     # Exist history script for Eval
     OE_Path = ROOT / 'storage' / 'compare_gpt_4' / '东方之星号游轮事件_gpt_4' / 'history.json'
 
