f7e6e827cff0bd41522297341e6cb1155588a522
lime728 <141900390+lime728@users.noreply.github.com>
2024-10-31 17:36:06 +0800

    Update api.py

--- api.py
+++ api.py
@@ -1,61 +1,24 @@
 import time
 from openai import OpenAI
 from config import Config
-from zhipuai import ZhipuAI
-
-ApiData = {
-    "OhMyGPT": {
-        "url": "https://cn2us02.opapi.win/v1/",
-        "key": "<Your Api Key Here>"
-    },
-    "GLM": {
-        "url": "https://open.bigmodel.cn/api/paas/v4/",
-        "key": "<Your Api Key Here>"
-    },
-    "Support": {
-        "url": "https://api.pumpkinaigc.online/v1",
-        "key": "<Your Api Key Here>"
-    }
-}
-
-'''
-OhMyGPT:
-[
-    "gpt-3.5-turbo-0125",
-    "gpt-4-1106-preview",
-    "gpt-4-turbo",
-    "gpt-4o"
-]
-ChatGLM:
-[
-    "glm-4",
-    "glm-4v",
-    "glm-3-turbo"
-]
-'''
 
 
 class Api:
     def __init__(
             self,
             model='gpt-4-1106-preview',
+            api='OpenAI',
             output_console=False
     ):
         self.model = model
-        if 'glm' in self.model:
-            self.url = ApiData['GLM']['url']
-            self.key = ApiData['GLM']['key']
-            self.client = ZhipuAI(
-                base_url=self.url,
-                api_key=self.key
-            )
-        else:
-            self.url = ApiData['Support']['url']    # Agent url
-            self.key = ApiData['Support']['key']    # Agent key
-            self.client = OpenAI(
-                base_url=self.url,
-                api_key=self.key
-            )
+        if model in Config.Small_LLM:
+            api = model
+        self.url = Config.ApiData[api]['url']
+        self.key = Config.ApiData[api]['key']
+        self.client = OpenAI(
+            base_url=self.url,
+            api_key=self.key
+        )
         self.gpt_params = {
             "temperature": 0.8,
             "top_p": 1,
@@ -68,16 +31,7 @@ class Api:
         self.output_console = output_console
 
     def run_api(self, message):
-        if self.model == Config.Eval_Model and Config.Eval_Model != Config.Model:
-            self.url = ApiData['OhMyGPT']['url']    # Eval url
-            self.key = ApiData['OhMyGPT']['key']    # Eval key
-            self.client = OpenAI(
-                base_url=self.url,
-                api_key=self.key
-            )
-            return self.run_with_client(message)
-        else:
-            return self.run_with_client(message)
+        return self.run_with_client(message)
 
     def run_with_client(self, message, stream=False):
         self.gpt_params['stream'] = stream
@@ -141,14 +95,13 @@ class Api:
 
 if __name__ == "__main__":
     message = 'Hello! How are you?'
-    # model_request = 'GPT4-turbo'
     model_client_1 = 'gpt-4o'
     model_client_2 = 'glm-4'
-    # Test run_with_request
-    # request_response = Api(model_request).run_with_request(message)
-    # print('\n{} request output: {}\n'.format(model_request, request_response))
     # Test run_with_client
-    client_response_1 = Api(model_client_1).run_with_client(message)
-    print('\n{} client output: {}\n'.format(model_client_1, client_response_1))
-    client_response_2 = Api(model_client_2).run_with_client(message)
-    print('\n{} client output: {}\n'.format(model_client_2, client_response_2))
+    # client_response_1 = Api(model_client_1).run_with_client(message)
+    # print('\n{} client output: {}\n'.format(model_client_1, client_response_1))
+    # client_response_2 = Api(model_client_2).run_with_client(message)
+    # print('\n{} client output: {}\n'.format(model_client_2, client_response_2))
+    print(Api(model='Qwen2-7B-Instruct',api='Qwen2').run_with_client(message))
+    # print(Api(model='glm-4-9b-chat',api='glm4').run_with_client(message))
+    # print(Api(model='Yi-1.5-9B-Chat',api='Yi1.5').run_with_client(message))
