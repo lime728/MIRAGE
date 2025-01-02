4409b2837e5d92eb8c7493d54f6d9bff4ec0d864
lime728 <141900390+lime728@users.noreply.github.com>
2025-01-02 16:40:18 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import sys
 import os
 import time
+import argparse
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]                          # root directory
@@ -9,67 +10,50 @@ if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))                  # add ROOT to PATH
 ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--ScriptNumList", default=[0, 1, 2, 3, 4, 5, 6, 7], nargs="+", type=int,
+                    help="Chosen Script Numbers to run (From 0 to 7)  -- LOWER PRIORITY!")
+parser.add_argument("--ScriptNum", default=None,
+                    help="Chosen Script Number (From 0 to 7 and None) -- HIGHER PRIORITY!")
+parser.add_argument("--Language", default="Zh", type=str, help="Script Language: ['En', 'Zh']")
+parser.add_argument("--MaxTurnNum", default=5, type=int, help="Runnable Turn Number")
+parser.add_argument("--MaxRetries", default=20, type=int, help="Max Retry in requests")
+parser.add_argument("--MaxBaseScriptSummaryToken", default=1500, type=int,
+                    help="Max token length of base script summary")
+parser.add_argument("--DEBUG", action="store_true", help="Debug Mode (Output all logs)")
+parser.add_argument("--Console", action="store_true", help="Console mode (Only output conversation contents)")
+parser.add_argument("--force_summary", action="store_true", help="Use force summary or not")
+parser.add_argument("--Only_Eval", action="store_true", help="Eval with exist history script")
+parser.add_argument("--OE_Path", default=None, type=str,
+                    help="Path of exist history script for only evaluation")
+parser.add_argument("--force_expose", action="store_true", help="Force to expose the culprits")
+parser.add_argument("--Base_Model", default="gpt-4-turbo", type=str,
+                    help="Api-based model for base functionality")
+parser.add_argument("--Culprit_Model", default='gpt-4-turbo', type=str, help="Api-based model for Culprits")
+parser.add_argument("--Civilian_Model", default='gpt-4-turbo', type=str, help="Api-based model for Civilians")
+parser.add_argument("--Eval_Model", default='gpt-4-turbo', type=str, help="Api based model for Evaluation")
+args = parser.parse_args()
+
 
 class Config:
-    # Manual Configs
-    ScriptNumList = [0, 1, 2, 3, 4, 5, 6, 7]    # Chosen Script Numbers to run (From 0 to 7)  -- LOWER PRIORITY!
-    ScriptNum = None                            # Chosen Script Number (From 0 to 7 and None) -- HIGHER PRIORITY!
-    Language = 'Zh'                             # Script Language: ['En', 'Zh']
-    MaxTurnNum = 5                              # Runnable Turn Number
-    MaxRetries = 20                             # Max Retry in requests
-    MaxBaseScriptSummaryToken = 1500            # Max token length of base script summary
-    DEBUG = False                               # debug mode (Output all logs)
-    Console = True                              # console mode (Only output conversation contents)
-    force_summary = True                        # Use force summary or not
-    Only_Eval = False                           # Eval with exist history script
-    force_expose = False                        # Force to expose the culprits
-
-    # Models
-    '''
-    Small Model:
-        Qwen2-7B-Instruct
-        Qwen1.5-7B-Chat
-        Qwen-7B-Chat
-        glm-4-9b-chat
-        Yi-1.5-9B-Chat
-    OpenAI Model:
-        gpt-4-turbo
-        gpt-3.5-turbo
-        gpt-4o
-    '''
-    Base_Model = 'Qwen2-7B-Instruct'            # Api-based model
-    Culprit_Model = 'Qwen2-7B-Instruct'         # Api-based model
-    Civilian_Model = 'Qwen2-7B-Instruct'        # Api-based model
-
-    # Eval Models
-    # Eval_Model = 'gpt-4-turbo'                  # Eval Api-based model
-    Eval_Model = 'Qwen2-7B-Instruct'                  # Eval Api-based model
-
     # API
     Small_LLM = [
         'Qwen2-7B-Instruct',
-        'Qwen1.5-7B-Chat',
-        'Qwen-7B-Chat',
-        'glm-4-9b-chat',
-        'Yi-1.5-9B-Chat'
     ]
     ApiData = {
         "OpenAI": {
-            "url": "<Your Url>",
-            "key": "<Your Key>"
+            "url": "<Your OpenAI URL>",
+            "key": "<Your OpenAI KEY>"
         },
         "Qwen2-7B-Instruct": {
-            "url": "<Your Url>",
-            "key": "<Your Key>"
+            "url": "<Your API URL>",
+            "key": "<Your API KEY>"
         }
     }
-
-    # Exist history script for Eval
-    OE_Path = None
-
+    
     # Base Directory
-    DataDir = ROOT / 'dataset' / Language
-    PromptDir = ROOT / 'prompts' / Language
+    DataDir = ROOT / 'dataset' / args.Language
+    PromptDir = ROOT / 'prompts' / args.Language
     LogDir = ROOT / 'logs'
     if not os.path.exists(LogDir):
         os.makedirs(LogDir)
@@ -81,7 +65,8 @@ class Config:
     ScriptDirs = [path for path in DataDir.iterdir() if not path.name.endswith('.json')]
     try:
         # Windows
-        ScriptNames = [str(path).split('\\')[2] for path in DataDir.iterdir() if not path.name.endswith('.json')]
+        ScriptNames = [str(path).split('\\')[2] for path in DataDir.iterdir() if
+                       not path.name.endswith('.json')]
     except:
         # Linux
         ScriptNames = [str(path).split('/')[2] for path in DataDir.iterdir() if not path.name.endswith('.json')]
@@ -102,3 +87,7 @@ class Config:
     LogPath = LogDir / ('log_' + time.strftime('%Y_%m_%d_%H_%M_%S', time.localtime(time.time())))
     if not os.path.exists(LogPath):
         os.makedirs(LogPath)
+
+for k, v in vars(args).items():
+    setattr(Config, k, v)
+
