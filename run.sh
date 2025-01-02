07b747ce53517b1f119048191a8527b4ac0f36fc
lime728 <141900390+lime728@users.noreply.github.com>
2025-01-02 16:45:54 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+python main.py --Console --force_summary \
+  --Base_Model gpt-4o-2024-08-06 \
+  --Culprit_Model gpt-4o-2024-08-06 \
+  --Civilian_Model gpt-4o-2024-08-06 \
+  --Eval_Model gpt-4-turbo \
+  --ScriptNumList 0 1 2 3 4 5 6 7 \
+  --ScriptNum None \
+  --Language Zh \
+  --MaxTurnNum 5 \
+  --MaxRetries 20 \
+  --MaxBaseScriptSummaryToken 1500
